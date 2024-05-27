74357e8d3913c984000f2a86bd187b9aecf3415d
enderneko <enderneko-dev@outlook.com>
2024-05-27 13:55:50 +0800

    Remove LibHealComm

--- PerfectRaid_IncomingHeals.lua
+++ PerfectRaid_IncomingHeals.lua
@@ -9,7 +9,7 @@
 
 
 local IncomingHeals = PerfectRaid:NewModule("PerfectRaid-IncomingHeals")
-local HealComm = LibStub("LibHealComm-4.0")
+-- local HealComm = LibStub("LibHealComm-4.0")
 
 local L = PerfectRaidLocals
 local utils, frames
@@ -40,18 +40,18 @@ end
 
 function IncomingHeals:EnableIncomingHeals(value)
 	if value then
-		-- self:RegisterEvent("UNIT_HEAL_PREDICTION", "UpdateIncomingHeals")
+		self:RegisterEvent("UNIT_HEAL_PREDICTION", "UpdateIncomingHeals")
 		-- self:RegisterEvent("UNIT_ABSORB_AMOUNT_CHANGED", "UpdateIncomingHeals")
-		HealComm.RegisterCallback(PerfectRaid.HealComm, "HealComm_HealStarted", "HealComm_UpdateHealPrediction")
-		HealComm.RegisterCallback(PerfectRaid.HealComm, "HealComm_HealUpdated", "HealComm_UpdateHealPrediction")
-		HealComm.RegisterCallback(PerfectRaid.HealComm, "HealComm_HealStopped", "HealComm_UpdateHealPrediction")
-		HealComm.RegisterCallback(PerfectRaid.HealComm, "HealComm_HealDelayed", "HealComm_UpdateHealPrediction")
-		HealComm.RegisterCallback(PerfectRaid.HealComm, "HealComm_ModifierChanged", "HealComm_UpdateHealPrediction")
-		HealComm.RegisterCallback(PerfectRaid.HealComm, "HealComm_GUIDDisappeared", "HealComm_UpdateHealPrediction")
+		-- HealComm.RegisterCallback(PerfectRaid.HealComm, "HealComm_HealStarted", "HealComm_UpdateHealPrediction")
+		-- HealComm.RegisterCallback(PerfectRaid.HealComm, "HealComm_HealUpdated", "HealComm_UpdateHealPrediction")
+		-- HealComm.RegisterCallback(PerfectRaid.HealComm, "HealComm_HealStopped", "HealComm_UpdateHealPrediction")
+		-- HealComm.RegisterCallback(PerfectRaid.HealComm, "HealComm_HealDelayed", "HealComm_UpdateHealPrediction")
+		-- HealComm.RegisterCallback(PerfectRaid.HealComm, "HealComm_ModifierChanged", "HealComm_UpdateHealPrediction")
+		-- HealComm.RegisterCallback(PerfectRaid.HealComm, "HealComm_GUIDDisappeared", "HealComm_UpdateHealPrediction")
 	else
-		-- self:UnregisterEvent("UNIT_HEAL_PREDICTION", "UpdateIncomingHeals")
+		self:UnregisterEvent("UNIT_HEAL_PREDICTION", "UpdateIncomingHeals")
 		-- self:UnregisterEvent("UNIT_ABSORB_AMOUNT_CHANGED", "UpdateIncomingHeals")
-		HealComm.UnregisterAllCallbacks(PerfectRaid.HealComm)
+		-- HealComm.UnregisterAllCallbacks(PerfectRaid.HealComm)
 	end	
 end
 
@@ -85,23 +85,13 @@ function IncomingHeals:UpdateButtonLayout( button )
 	
 end
 
-function IncomingHeals:UpdateIncomingHeals( event, target, guid)
-
-	if UnitGUID(target) ~= guid then return end
-	
+function IncomingHeals:UpdateIncomingHeals( event, target )
 	local health = UnitHealth(target)
 	local maxhealth = UnitHealthMax(target)
-	local healinc = 0
+	local healinc = UnitGetIncomingHeals(target)
 	
-	-- not correct healinc or health
 	if not health then return end
 
-	local modifier = HealComm:GetHealModifier(guid) or 1
-	healinc = (HealComm:GetHealAmount(guid, HealComm.CASTED_HEALS) or 0) * modifier
-	-- NOTE: hots within 3 seconds
-	hot = (HealComm:GetHealAmount(guid, HealComm.OVERTIME_AND_BOMB_HEALS, GetTime()+3) or 0) * modifier
-	healinc = healinc + hot
-	
 	local healthincsum = health + healinc
 	
 	-- adjust healthsum to maxhealth
@@ -122,23 +112,62 @@ function IncomingHeals:UpdateIncomingHeals( event, target, guid)
 			
 		end			
 	end
-	
-	
 end
+
+-- function IncomingHeals:UpdateIncomingHeals( event, target, guid)
+
+-- 	if UnitGUID(target) ~= guid then return end
+	
+-- 	local health = UnitHealth(target)
+-- 	local maxhealth = UnitHealthMax(target)
+-- 	local healinc = 0
+	
+-- 	-- not correct healinc or health
+-- 	if not health then return end
+
+-- 	local modifier = HealComm:GetHealModifier(guid) or 1
+-- 	healinc = (HealComm:GetHealAmount(guid, HealComm.CASTED_HEALS) or 0) * modifier
+-- 	-- NOTE: hots within 3 seconds
+-- 	hot = (HealComm:GetHealAmount(guid, HealComm.OVERTIME_AND_BOMB_HEALS, GetTime()+3) or 0) * modifier
+-- 	healinc = healinc + hot
+	
+-- 	local healthincsum = health + healinc
+	
+-- 	-- adjust healthsum to maxhealth
+-- 	if healthincsum > maxhealth then healthincsum = maxhealth end
+	
+-- 	for unit, tbl in pairs(frames) do
+-- 		if UnitIsUnit( target, unit ) then
+-- 			for frame in pairs(frames[unit]) do	
+-- 				-- heal inc
+-- 				if healinc == 0 or health == maxhealth then				
+-- 					frame.incominghealsbar:Hide()
+-- 				else
+-- 					frame.incominghealsbar:SetMinMaxValues(0, maxhealth)
+-- 					frame.incominghealsbar:SetValue(healthincsum)
+-- 					frame.incominghealsbar:Show()
+-- 				end
+-- 			end
+			
+-- 		end			
+-- 	end
+	
+	
+-- end
 
 -------------------------------------------------
 -- LibHealComm
 -------------------------------------------------
-PerfectRaid.HealComm = {}
-local function HealComm_UpdateHealPrediction(_, event, casterGUID, spellID, healType, endTime, ...)
-    -- update incomingHeal
-    for i = 1, select("#", ...) do
-		local guid = select(i, ...)
-        local unit = PerfectRaid.guidToUnit[guid]
-        if unit then
-			-- print(event, casterGUID, spellID, healType, endTime, ...)
-            IncomingHeals:UpdateIncomingHeals(event, unit, guid)
-        end
-    end
-end
-PerfectRaid.HealComm.HealComm_UpdateHealPrediction = HealComm_UpdateHealPrediction
+-- PerfectRaid.HealComm = {}
+-- local function HealComm_UpdateHealPrediction(_, event, casterGUID, spellID, healType, endTime, ...)
+--     -- update incomingHeal
+--     for i = 1, select("#", ...) do
+-- 		local guid = select(i, ...)
+--         local unit = PerfectRaid.guidToUnit[guid]
+--         if unit then
+-- 			-- print(event, casterGUID, spellID, healType, endTime, ...)
+--             IncomingHeals:UpdateIncomingHeals(event, unit, guid)
+--         end
+--     end
+-- end
+-- PerfectRaid.HealComm.HealComm_UpdateHealPrediction = HealComm_UpdateHealPrediction
